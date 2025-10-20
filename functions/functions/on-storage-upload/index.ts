0065834db5bd2969e88105d5bde70c0541a585e4
papicandela <basilis.fernando@gmail.com>
2025-10-20 12:26:13 -0400

    Reverted indext.ts

--- functions/functions/on-storage-upload/index.ts
+++ functions/functions/on-storage-upload/index.ts
@@ -39,7 +39,7 @@ serve(async (req: Request) => {
     }
 
     // Inicializar cliente de Supabase
-    const supabaseUrl = Deno.env.get("SUPABASE_PUBLIC_URL")!;
+    const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
     const supabaseKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
     const supabase = createClient(supabaseUrl, supabaseKey);
 
